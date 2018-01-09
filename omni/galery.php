483ea843dfe81b465a8613611b8ffdca322e5da5
a hafidz dias insyira <insyiracreation@gmail.com>
2018-01-09 11:42:28 +0700

    Update galery.php

--- omni/galery.php
+++ omni/galery.php
@@ -14,105 +14,104 @@
 </head>
 <body>
 
-<div id="title-galery" class="container">
-	<div class="row">
-		<div class="col-md-4">
-			<div class="Galeri">
-				Galeri
+	<div id="title-galery" class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="Galeri">
+					Galeri
+				</div>
 			</div>
-		</div>
-		<div class="col-md-8">
-			<div class="text">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et set dolore magna un aliqua. Ut enim ad minim veniam, quis  deur nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum sit error dolore eu fugiat nulla pariatur. Culpa Excepteur sint occaecat cupidatat non proident, sunt in culpa qui natus officia deserunt mollit anim id est laborum Sed ut perspiciatis unde omnis iste natus error sit un.
+			<div class="col-md-8">
+				<div class="text">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et set dolore magna un aliqua. Ut enim ad minim veniam, quis  deur nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum sit error dolore eu fugiat nulla pariatur. Culpa Excepteur sint occaecat cupidatat non proident, sunt in culpa qui natus officia deserunt mollit anim id est laborum Sed ut perspiciatis unde omnis iste natus error sit un.
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-<div class="line"></div>
+	<div class="line"></div>
 
-<div id="foto-galeri" class="container">
-	<div class="title-foto">
-		Photos Galery
-	</div>
-	<div class="row">
-
-		<div class="col-md-4 col-sm-6">
-			<div class="img-box">
-				<img src="img/1.jpg" class="img">
-			</div>
-			<div class="detail-img">
-				<div class="title-detail">
-					Moon Fever
-				</div>
-				<div class="text">
-					Duis aute irure dolor in reprehenderit.
-				</div>
-			</div>
+	<div id="foto-galeri" class="container">
+		<div class="title-foto">
+			Photos Galery
 		</div>
+		<div class="row">
 
-		<div class="col-md-4 col-sm-6">
-			<div class="img-box">
-				<img src="img/2.jpg" class="img">
-			</div>
-			<div class="detail-img">
-				<div class="title-detail">
-					Moon Fever
+			<div class="col-md-4 col-sm-6">
+				<div class="img-box">
+					<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModalfoto"> <img src="img/can2.jpg " width="382	" height="330" class="img"></a>
 				</div>
-				<div class="text">
-					Duis aute irure dolor in reprehenderit.
+				<div class="detail-img">
+					<div class="title-detail">
+						Candra Malik Syahadat Cinta
+					</div>
+					<div class="text">
+						Duis aute irure dolor in reprehenderit.
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="col-md-4 col-sm-6">
-			<div class="img-box">
-				<img src="img/3.jpg" class="img">
-			</div>
-			<div class="detail-img">
-				<div class="title-detail">
-					Moon Fever
+			<div class="col-md-4 col-sm-6">
+				<div class="img-box">
+					<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModalfoto"> <img src="img/can2.jpg " class="img"></a>
 				</div>
-				<div class="text">
-					Duis aute irure dolor in reprehenderit.
+				<div class="detail-img">
+					<div class="title-detail">
+						Candra Malik Syahadat Cinta
+					</div>
+					<div class="text">
+						Duis aute irure dolor in reprehenderit.
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="col-md-4 col-sm-6">
-			<div class="img-box">
-				<img src="img/4.jpg" class="img">
-			</div>
-			<div class="detail-img">
-				<div class="title-detail">
-					Moon Fever
+			<div class="col-md-4 col-sm-6">
+				<div class="img-box">
+					<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModalfoto"> <img src="img/can2.jpg " class="img"></a>
 				</div>
-				<div class="text">
-					Duis aute irure dolor in reprehenderit.
+				<div class="detail-img">
+					<div class="title-detail">
+						Candra Malik Syahadat Cinta
+					</div>
+					<div class="text">
+						Duis aute irure dolor in reprehenderit.
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="col-md-4 col-sm-6">
-			<div class="img-box">
-				<img src="img/1.jpg" class="img">
-			</div>
-			<div class="detail-img">
-				<div class="title-detail">
-					Moon Fever
+			<div class="col-md-4 col-sm-6">
+				<div class="img-box">
+					<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModalfoto"> <img src="img/can2.jpg " class="img"></a>
 				</div>
-				<div class="text">
-					Duis aute irure dolor in reprehenderit.
+				<div class="detail-img">
+					<div class="title-detail">
+						Candra Malik Syahadat Cinta
+					</div>
+					<div class="text">
+						Duis aute irure dolor in reprehenderit.
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="col-md-4 col-sm-6">
-			<div class="img-box">
-				<img src="img/2.jpg" class="img">
+			<div class="col-md-4 col-sm-6">
+				<div class="img-box">
+					<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModalfoto"> <img src="img/can2.jpg " class="img"></a>
+				</div>
+				<div class="detail-img">
+					<div class="title-detail">
+						Candra Malik Syahadat Cinta
+					</div>
+					<div class="text">
+						Duis aute irure dolor in reprehenderit.
+					</div>
+				</div>
 			</div>
-			<div class="detail-img">
+
+			<div class="col-md-4 col-sm-6">
+				<div class="img-box">
+					<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModalfoto"> <img src="img/can2.jpg " class="img"></a>
+				</div>
 				<div class="title-detail">
-					Moon Fever
+					Candra Malik Syahadat Cinta
 				</div>
 				<div class="text">
 					Duis aute irure dolor in reprehenderit.
@@ -139,7 +138,58 @@
 				<div class="long-time">
 					02:32
 				</div>
-				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/1.jpg " class="img"></a>
+				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/can1.jpg " class="img"></a>
+			</div>
+			<div class="detail-img">
+				<div class="title-detail">
+					Candra Malik Syahadat Cinta
+				</div>
+				<div class="text">
+					Duis aute irure dolor in reprehenderit.
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-4 col-sm-6">
+			<div class="img-box">
+				<div class="long-time">
+					02:32
+				</div>
+				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/can1.jpg " class="img"></a>
+			</div>
+			<div class="detail-img">
+				<div class="title-detail">
+					Candra Malik Syahadat Cinta
+				</div>
+				<div class="text">
+					Duis aute irure dolor in reprehenderit.
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-4 col-sm-6">
+			<div class="img-box">
+				<div class="long-time">
+					02:32
+				</div>
+				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/can1.jpg " class="img"></a>
+			</div>
+			<div class="detail-img">
+				<div class="title-detail">
+					Candra Malik Syahadat Cinta
+				</div>
+				<div class="text">
+					Duis aute irure dolor in reprehenderit.
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-4 col-sm-6">
+			<div class="img-box">
+				<div class="long-time">
+					02:32
+				</div>
+				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/can1.jpg " class="img"></a>
 			</div>
 			<div class="detail-img">
 				<div class="title-detail">
@@ -156,11 +206,11 @@
 				<div class="long-time">
 					02:32
 				</div>
-				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/1.jpg " class="img"></a>
+				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/can1.jpg " class="img"></a>
 			</div>
 			<div class="detail-img">
 				<div class="title-detail">
-					Moon Fever
+					Candra Malik Syahadat Cinta
 				</div>
 				<div class="text">
 					Duis aute irure dolor in reprehenderit.
@@ -173,62 +223,11 @@
 				<div class="long-time">
 					02:32
 				</div>
-				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/1.jpg " class="img"></a>
+				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/can1.jpg " class="img"></a>
 			</div>
 			<div class="detail-img">
 				<div class="title-detail">
-					Moon Fever
-				</div>
-				<div class="text">
-					Duis aute irure dolor in reprehenderit.
-				</div>
-			</div>
-		</div>
-
-		<div class="col-md-4 col-sm-6">
-			<div class="img-box">
-				<div class="long-time">
-					02:32
-				</div>
-				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/1.jpg " class="img"></a>
-			</div>
-			<div class="detail-img">
-				<div class="title-detail">
-					Moon Fever
-				</div>
-				<div class="text">
-					Duis aute irure dolor in reprehenderit.
-				</div>
-			</div>
-		</div>
-
-		<div class="col-md-4 col-sm-6">
-			<div class="img-box">
-				<div class="long-time">
-					02:32
-				</div>
-				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/1.jpg " class="img"></a>
-			</div>
-			<div class="detail-img">
-				<div class="title-detail">
-					Moon Fever
-				</div>
-				<div class="text">
-					Duis aute irure dolor in reprehenderit.
-				</div>
-			</div>
-		</div>
-
-		<div class="col-md-4 col-sm-6">
-			<div class="img-box">
-				<div class="long-time">
-					02:32
-				</div>
-				<a href="#" class="btn btn-download" data-toggle="modal" data-target="#myModal"> <img src="img/1.jpg " class="img"></a>
-			</div>
-			<div class="detail-img">
-				<div class="title-detail">
-					Moon Fever
+					Candra Malik Syahadat Cinta
 				</div>
 				<div class="text">
 					Duis aute irure dolor in reprehenderit.
@@ -243,38 +242,56 @@
 	</div>
 </div>
 
-<!-- Modal -->
-    <div id="myModal" class="modal fade" role="dialog">
-        <div class="modal-dialog">
-            <!-- konten modal-->
-            <div class="modal-content">
-                <!-- heading modal -->
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Bagian heading modal</h4>
-                </div>
-                <!-- body modal -->
-                <div class="modal-body">
-                    <!-- 16:9 aspect ratio -->
-					<div class="embed-responsive embed-responsive-16by9">
-					 <iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=x_2XgLON5d8"></iframe>
-					</div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">VIDEO GALLEY</h4>
+			</div>
+			<div class="modal-body">
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe src="https://www.youtube.com/embed/ELztd70LZIA" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
 
-					<!-- 4:3 aspect ratio -->
-					<div class="embed-responsive embed-responsive-4by3">
-					 <iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=x_2XgLON5d8"></iframe>
+<!-- Modal -->
+<div class="modal fade" id="myModalfoto" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">	
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Gallery Foto</h4>
+			</div>
+			<div class="modal-body">
+				<div class="row">
+					<div class="col-md-6">
+						<img src="img/can2.jpg" width="1800" height="1800" alt="" class="img-responsive">
 					</div>
-                </div>
-                <!-- footer modal -->
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Tutup Modal</button>
-                </div>
-            </div>
-        </div>
-    </div>
-   </div>        
-<script src="https://code.jquery.com/jquery.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+					<div class="col-md-6">
+						<h2>Candra Malik</h2>
+						<p> Gambar ini dibuat oleh seorang fans dari candra malik, ia sangat menyukai karya-karya candra malik sehingga ia membuat effect paint gambar candra malik </p> 
+						
+					</div>
+				</div>		
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>	
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
 
 </body>
 </html>
